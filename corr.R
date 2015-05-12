8bbe6b32a149535349b0bd0be9d3b38bd321e95e
Shu <shuwang.uw@gmail.com>
2015-05-12 00:36:22 -0700

    Create corr.R

---
+++ corr.R
@@ -0,0 +1,20 @@
+## R Programming Assignment 1.3
+corr <- function(directory, threshold = 0){
+  setwd(paste("~/R Programming/", directory, sep = ""))
+  dt <- complete(directory, 1:322)
+  idt <- c(dt$id[dt$nobs >= threshold])
+  
+  correlation <- c()
+  for (i in idt){
+    if(i <= 9){
+      data <- read.csv(paste("00", i, ".csv", sep = ""))
+    }else if(i >= 10 && i <= 99){
+      data <- read.csv(paste("0", i, ".csv", sep = ""))
+    }else{
+      data <- read.csv(paste(i, ".csv", sep = ""))
+    }
+    complete_data <- data[complete.cases(data), ]
+    correlation <- c(correlation, cor(complete_data$sulfate, complete_data$nitrate))
+  }
+  correlation
+}
