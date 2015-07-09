3e98f3a8cbd2057a7620ebce331aca011779da61
Shu <shuwang.uw@gmail.com>
2015-07-08 20:33:24 -0700

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,19 +1,31 @@
+## R-Programming Project1.2: correlation
+## by Shu Wang
 corr <- function(directory, threshold = 0){
-  setwd(paste("~/R Programming/", directory, sep = ""))
+
   dt <- complete(directory, 1:332)
-  idt <- c(dt$id[dt$nobs > threshold])
   
+  ## subset the data
+  sub <- subset(dt, nobs >= threshold, select = id)
+  
+  ## convert the data frame to vector
+  sub_id <- sub$id
+  
+  ## initialize an empty vector
   correlation <- c()
-  for (i in idt){
-    if(i <= 9){
-      data <- read.csv(paste("00", i, ".csv", sep = ""))
-    }else if(i >= 10 && i <= 99){
-      data <- read.csv(paste("0", i, ".csv", sep = ""))
-    }else{
-      data <- read.csv(paste(i, ".csv", sep = ""))
-    }
-    complete_data <- data[complete.cases(data), ]
-    correlation <- c(correlation, cor(complete_data$sulfate, complete_data$nitrate))
+  
+  for (i in sub_id){
+    
+    ## set file path for files identified by "i"
+    path <- file.path(directory, sprintf("%03d.csv", as.numeric(i)))
+    
+    ## read data in each data file
+    file <- read.csv(path)
+    
+    ## remove the NA
+    complete_file <- file[complete.cases(file), ]
+    
+    ## calculate and store the correlation to vector that initialized before
+    correlation <- c(correlation, cor(complete_file$sulfate, complete_file$nitrate)) 
   }
   correlation
 }
